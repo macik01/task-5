6dc3e079df0822ef0df5fd6f3fd8ef8f29c3262f
Admin <macik9157@gmail.com>
2024-10-23 12:24:53 +0300

    ДЗ 5.1. 5/2 Ім'я змінної

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import string
 import keyword
 
 #task 5/1
-smina = input("ведіть зміну")
+'''smina = input("ведіть зміну")
 if smina in keyword.kwlist:
     print("False")
 elif smina.isupper() or smina in string.whitespace or (smina in string.punctuation and smina != '_'):
@@ -11,6 +11,35 @@ elif smina.count("_") > 1:
     print ("False")
 elif smina[0].isdigit():
     print("False")
-else: print("True")
+else: print("True")'''
+#task 5/2
+while True:
+
+    operation = input("Якщо потрібно рахувати, введіть 'Yes', якщо ні — інше: ")
+    lower_operation = operation.lower()
+    if lower_operation not in ['yes', 'y']:
+        print("Exit")
+        break
+    num_one = int(input("Введіть перше число: "))
+    num_two = int(input("Введіть друге число: "))
+    num_oper = input("Введіть операцію ('+', '-', '*', '/'): ")
+    if num_oper == '+':
+        res = num_one + num_two
+        print(f"Результат: {res}")
+    elif num_oper == '-':
+        res = num_one - num_two
+        print(f"Результат: {res}")
+    elif num_oper == '*':
+        res = num_one * num_two
+        print(f"Результат: {res}")
+    elif num_oper == '/':
+        if num_two != 0:
+            res = num_one / num_two
+            print(f"Результат: {res}")
+        else:
+            print("Помилка: Ділення на нуль!")
+    else:
+        print("Помилка: Невірна операція.")
+
 
 
